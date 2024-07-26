bf62043c4caaabe27ad7afd723913bc80a75399e
niaDev <estefania.atencioramirez@gmail.com>
2024-07-25 23:16:08 -0300

    Agregando bootstrap

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 //const
+
 const clientes = []
 const buscarReporte = document.getElementById("buscarReporte")
 const listaResultado = document.getElementById("listaResultado")
 const noResultados = document.getElementById("noresultados")
 
 cargarLocalStorage()
-//objeto
 
+//objeto
+let porcentaje = 1
 class Cliente {
-    static id = 0
+    
     constructor (nombre, email, dni, depto, ingresos, montoPrestamo, nroCuotas){
-        this.id = ++Cliente.id,
+        this.id = clientes.length + 1,
         this.nombre = nombre,
         this.email = email,
         this.dni = dni,
@@ -21,7 +23,19 @@ class Cliente {
         this.montoCuotas = this.getMontoCuota()
     }
     getMontoCuota = () => {
-        return Number(this.montoPrestamo)/Number(this.nroCuotas)
+        if (DataBankToday != null){
+            porcentaje = (100+DataBankToday.tasa)/100
+        }
+        return Number((this.montoPrestamo)*porcentaje)/Number(this.nroCuotas)
+    }
+}
+
+class DataBank {
+    constructor (nombre, pais, fechaActuallizacion, tasa){
+        this.nombre = nombre,
+        this.pais = pais,
+        this.fechaActuallizacion = fechaActuallizacion,
+        this.tasa = tasa
     }
 }
 
@@ -60,6 +74,33 @@ function mostrarReporte(Cliente) {
         document.getElementById("reporteDepto").innerHTML = Cliente.depto;
         document.getElementById("reporteMonto").innerHTML = Cliente.montoPrestamo;
         document.getElementById("reporteCuotas").innerHTML = Cliente.montoCuotas.toFixed(2);
+}
+
+
+function crearReportesCartas(clientes){
+
+    clientes.forEach(element => {
+        const div = document.createElement('div');
+        div.classList.add('col-lg-3','col-md-4','mb-4', 'tarjeta')
+          div.innerHTML = `
+            <div class="card" style="width: 30rem;">
+                <div class="card-header">
+                    Id Reporte : ${element.id}
+                </div>
+                <ul class="list-group list-group-flush">
+                <li class="list-group-item">Nombre y Apellido: ${element.nombre}</li>
+                <li class="list-group-item">Email: ${element.email}</li>
+                <li class="list-group-item">DNI: ${element.dni}</li>
+                <li class="list-group-item">Departamento: ${element.depto}</li>
+                <li class="list-group-item">Monto: ${element.montoPrestamo}</li>
+                <li class="list-group-item">Cuotas: ${element.montoCuotas}</li>
+                </ul>
+            </div>
+        `
+        document.getElementById("reporteUsingCreate").appendChild(div)
+    })
+
+    document.getElementById("reporteUsingCreate").style.display = "block";
 }
 
 //Ver todos los reportes
@@ -154,14 +195,14 @@ function crearReportes(Cliente) {
 
 function verTodosLosReportes(){
     document.getElementById("reporteCompleto").style.display = "none";
+   
     let clientesLocal = obtenerLocalStorage()
-    clientesLocal.forEach(cliente => {
-        
-        crearReportes(cliente)
-        
-    });
+    if (clientesLocal != null){
+        crearReportesCartas(clientesLocal)
+    }else{
+        //TODO nnotificar que nno hay reportes
+    }
 }
-
 
 
 //filtrar 1 reporte
@@ -182,8 +223,6 @@ const usuarioBusqueda = () => {
         })
         noResultados.style.display = "none"
     }
-
-    
 
     if(buscarReporte.value === ""){
         listaResultado.innerHTML = ""   
@@ -223,7 +262,43 @@ function vaciarFormulario() {
   document.getElementById("reporteUsingCreate").style.display = "none"
 }
 
+let DataBankToday
+
+//fetch reportes json
+function getInterestRate(e){
+    let pais = e.srcElement.innerHTML
+    const request = new Request("https://api.api-ninjas.com/v1/interestrate?country="+pais, {
+        method: "GET",
+        headers: { 'X-Api-Key': 'pJp+tnL6QVv74yBvC1Q+jA==rWk9amRVGRaO19lr'},
+        contentType: 'application/json',
+      });
+      
+    fetch(request)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then( rates => {
+        
+        data = rates.central_bank_rates[0]
+        
+        DataBankToday = new DataBank(data.central_bank, data.country, data.last_updated,data.rate_pct)
+        document.getElementById("paisSeleccioado").innerHTML = DataBankToday.pais
+        banco.innerHTML = DataBankToday.nombre
+        tasa.innerHTML = DataBankToday.tasa
+        updated.innerHTML = DataBankToday.fechaActuallizacion
+        datosBanco.style.display = "block"
+      })
+      .catch(e => console.log(e));
+}
+
+function vaciarLocalStorage(){
+    localStorage.clear()
+}
+
+
+
 //let, events, dom
+
 let miReporte = document.getElementById("miReporte")
 miReporte.addEventListener("submit", ingresarDatos)
 
@@ -240,3 +315,17 @@ let borrarFormulario = document.getElementById("vaciar")
 borrarFormulario.addEventListener("click", vaciarFormulario)
 
 
+let botonAustralia = document.getElementById("Australia")
+botonAustralia.addEventListener("click", getInterestRate)
+let botonIglaterra = document.getElementById("Inglaterra")
+botonIglaterra.addEventListener("click", getInterestRate)
+
+let datosBanco = document.getElementById("datosBanco")
+datosBanco.style.display = "none"
+
+let vaciarLocal = document.getElementById("vaciarLocal")
+vaciarLocal.addEventListener("click", vaciarLocalStorage)
+
+let banco = document.getElementById("banco")
+let tasa = document.getElementById("tasa")
+let updated = document.getElementById("updated")
